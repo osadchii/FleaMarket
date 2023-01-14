6f7592520e4b4008ec3e2d5ffe52e618b2057705
a.osadchii <roflandcom@gmail.com>
2023-01-14 21:09:30 +0200

    telegram bot owner id is required

---
+++ src/FleaMarket.Data/Migrations/20230114190806_TelegramBotOwnerIsRequired.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FleaMarket.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class TelegramBotOwnerIsRequired : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_telegram_bot_telegram_user_OwnerId",
+                schema: "flea_market",
+                table: "telegram_bot");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "OwnerId",
+                schema: "flea_market",
+                table: "telegram_bot",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_telegram_bot_telegram_user_OwnerId",
+                schema: "flea_market",
+                table: "telegram_bot",
+                column: "OwnerId",
+                principalSchema: "flea_market",
+                principalTable: "telegram_user",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_telegram_bot_telegram_user_OwnerId",
+                schema: "flea_market",
+                table: "telegram_bot");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "OwnerId",
+                schema: "flea_market",
+                table: "telegram_bot",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_telegram_bot_telegram_user_OwnerId",
+                schema: "flea_market",
+                table: "telegram_bot",
+                column: "OwnerId",
+                principalSchema: "flea_market",
+                principalTable: "telegram_user",
+                principalColumn: "Id");
+        }
+    }
+}
