4accc0ffee0e95bdff94221c93224917f0c5fa53
Anton Osadchii <roflandcom@gmail.com>
2023-01-20 20:29:55 +0200

    main menu activate test

---
+++ tests/FleaMarket.Tests/StateHandlers/Management/MainMenuState/MainMenuStateHandler.cs
@@ -0,0 +1,31 @@
+﻿using FleaMarket.Infrastructure.StateHandlers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FleaMarket.Tests.StateHandlers.Management.MainMenuState;
+
+public class MainMenuStateHandler : TestContext
+{
+    private readonly IStateHandler<Infrastructure.StateHandlers.Management.MainMenu.MainMenuState, string> _mainMenuStateHandler;
+    
+    public MainMenuStateHandler(TestWebApplicationFactory factory) : base(factory)
+    {
+        _mainMenuStateHandler = Services.GetRequiredService<IStateHandler<Infrastructure.StateHandlers.Management.MainMenu.MainMenuState, string>>();
+    }
+
+    [Fact]
+    public async Task ShouldBeActivated()
+    {
+        // Arrange
+
+        var user = await CreateTelegramUser();
+
+        // Act
+
+        await _mainMenuStateHandler.Activate(user.Id, null,
+            Infrastructure.StateHandlers.Management.MainMenu.MainMenuState.Default);
+
+        // Assert
+
+        await this.ValidateMainMenuStateActivate(user.ChatId);
+    }
+}
